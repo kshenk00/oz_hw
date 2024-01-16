c05adc9d0e45d4fdf1105c6f596bcba325209f69
kshenk00 <00byoon@gmail.com>
2024-01-17 01:54:21 +0900

    타입스크립트 환경설정과 옵션들

---
+++ TimeScript study/chapter1/dist/index.js
@@ -0,0 +1,3 @@
+"use strict";
+//최상위 폴더에 index.ts 파일 만들어주기
+console.log("Hello TypeSript");
---
+++ TimeScript study/chapter1/src/index.ts
@@ -0,0 +1,12 @@
+//최상위 폴더에 index.ts 파일 만들어주기
+
+//console.log("Hello TypeSript");
+
+
+//const nums: number[] = [];
+//const a = 10 + nums[0]
+
+//💡ES5에는 존재하지 않는 문법을 사용을 해준다고 할때 : endsWith
+const someString = "typtsrcipt!";
+someString.endsWith("!") 
+/*endsWith메소드는 string 메소드. 그래서 string을 검사해서 여기에 있는 마지막 글자가 내가 지정한 글자와 일치하느냐는 메소드*/
