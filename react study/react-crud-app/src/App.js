2398e418558fa9cef08588e911567c36401f9b00
kshenk00 <00byoon@gmail.com>
2024-01-24 01:23:52 +0900

    form and list

--- react study/react-crud-app/src/App.js
+++ react study/react-crud-app/src/App.js
@@ -1,5 +1,7 @@
 import { Component } from "react";
 import "./App.css";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
 
 export default class App extends Component {
   render() {
@@ -8,12 +10,13 @@ export default class App extends Component {
     <div className="sub-container">
       <h1>장바구니</h1>
       <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
-        { /* Expense Form */} Expense Form
-       
+        { /* Expense Form */} 
+        <ExpenseForm />
       </div>
       
       <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
-        { /* Expense List */} Expense List
+        { /* Expense List */}
+        <ExpenseList />
        
       </div>
 
---
+++ react study/react-crud-app/src/components/ExpenseList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './ExpenseList.css';
+
+export default class ExpenseList extends Component {
+  render() {
+    return (
+      <>
+        <ul className='list'>
+  
+        </ul>
+
+        <button className='btn'>
+          목록 지우기
+        </button>
+
+        </>
+    )
+  }
+}
